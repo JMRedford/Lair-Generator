3fb3915a7c85b11aca9cdcd751da7f2912426e55
john <redford.john.m@gmail.com>
2015-08-16 09:04:01 -0500

    added random package

--- Lair.py
+++ Lair.py
@@ -1,14 +1,34 @@
-import pygame, time
+import pygame, time, random
+
+xBlocks = 25
+yBlocks = 20
+blockSize = 20
+
+GREY = (200,200,200)
 
 pygame.init()
+disp = pygame.display.set_mode([xBlocks*blockSize,yBlocks*blockSize],pygame.NOFRAME)
+
+model = [];
+
+for i in range(0,xBlocks):
+  model.append([])
+  for j in range(0,yBlocks):
+    model[i].append([pygame.Rect(i*blockSize,j*blockSize,blockSize,blockSize),0])
+
+model[12][18][1] = 1
 
 def updateModel():
   print('update model')
 
 def updateView():
-  print('update view')
-
-pygame.display.set_mode([500,400],pygame.NOFRAME)
+  for i in range(0,25):
+    for j in range(0,20):
+      if model[i][j][1] == 1:
+        surf = pygame.Surface((blockSize,blockSize))
+        surf.fill(GREY)
+        disp.blit(surf,model[i][j][0])
+  pygame.display.flip()
 
 while(1):
   e = pygame.event.poll()
