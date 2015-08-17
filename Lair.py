f1b3588eebd3ec2819a621772e9f96d02a49c378
john <redford.john.m@gmail.com>
2015-08-17 15:01:52 -0500

    generating maze

--- Lair.py
+++ Lair.py
@@ -1,29 +1,50 @@
-import pygame, time, random
+import pygame, time, random, math
 
-xBlocks = 25
-yBlocks = 20
-blockSize = 20
+random.seed()
+
+xBlocks = 50
+yBlocks = 40
+blockSize = 10
 
 GREY = (200,200,200)
 
 pygame.init()
 disp = pygame.display.set_mode([xBlocks*blockSize,yBlocks*blockSize],pygame.NOFRAME)
 
-model = [];
+model = []
+probs = [[1,0,0,0],[0.5,0.22,0.06,0.22],[0.45,0.23,0.09,0.23],[0.4,0.23,0.14,0.23],[0.35,0.23,0.19,0.23],[0.3,0.24,0.22,0.24],[0.25,0.25,0.25,0.25]]
 
 for i in range(0,xBlocks):
   model.append([])
   for j in range(0,yBlocks):
     model[i].append([pygame.Rect(i*blockSize,j*blockSize,blockSize,blockSize),0])
 
-model[12][18][1] = 1
+model[xBlocks/2][yBlocks-2][1] = 1
 
 def updateModel():
+  pos = (xBlocks/2,yBlocks-2)
+  lastDir = (0,-1)
+  while model[pos[0]][pos[1]][1] > 0:
+    randNum = random.random()
+    if randNum < probs[min(yBlocks-2-pos[1],6)][0]:
+      lastDir = (0,-1)
+      pos = (pos[0],pos[1]-2)
+    elif randNum < probs[min(yBlocks-2-pos[1],6)][0] + probs[min(yBlocks-2-pos[1],6)][1]:
+      lastDir = (1,0)
+      pos = (pos[0]+2,pos[1])
+    elif randNum < probs[min(yBlocks-2-pos[1],6)][0] + probs[min(yBlocks-2-pos[1],6)][1] + probs[min(yBlocks-2-pos[1],6)][2]:
+      lastDir = (0,1)
+      pos = (pos[0],pos[1]+2)
+    else:  
+      lastDir = (-1,0)
+      pos = (pos[0]-2,pos[1])
+  model[pos[0]][pos[1]][1] = 1
+  model[pos[0]-lastDir[0]][pos[1]-lastDir[1]][1] = 1
   print('update model')
 
 def updateView():
-  for i in range(0,25):
-    for j in range(0,20):
+  for i in range(0,xBlocks):
+    for j in range(0,yBlocks):
       if model[i][j][1] == 1:
         surf = pygame.Surface((blockSize,blockSize))
         surf.fill(GREY)
